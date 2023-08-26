1fe0bdd7a517256ca42195119778f9767bdd5788
rafinhahdc19 <124531634+rafinhahdc19@users.noreply.github.com>
2023-08-26 17:55:30 -0300

    modification build 2

--- src/server.js
+++ src/server.js
@@ -8,8 +8,9 @@ app.use(express.json())
 app.use(routes)
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://haveal-frontend.vercel.app");
-    next();
+    //res.header("Access-Control-Allow-Origin", "https://haveal-frontend.vercel.app");
+    res.status(200).json("ta aqui")
+    //next();
 });
 
 
