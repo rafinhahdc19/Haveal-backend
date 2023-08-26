ea260fd92e1984e524b5001e08284e4430d96473
rafinhahdc19 <124531634+rafinhahdc19@users.noreply.github.com>
2023-08-26 17:43:56 -0300

    modification build 2

--- src/server.js
+++ src/server.js
@@ -7,12 +7,16 @@ const PORT = 80
 app.use(express.json())
 app.use(routes)
 
-app.use((req,res,next) => {
-    res.header("Access-Control-Allow-Origin", "https://haveal-frontend.vercel.app")
-    app.use(cors());
-    next()
-})
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://haveal-frontend.vercel.app");
+    next();
+});
 
+
+app.use(cors({
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+}));
 app.post('/', (req, res) => {
     const { numero1, numero2 } = req.body
     res.status(200).json(numero1 + numero2)
